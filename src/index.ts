2153aa2f6f3faf00892ccac932a163e7ca910d03
mehdi <legoullonmehdi@gmail.com>
2020-03-22 00:14:26 +0100

    improve env folder and clean code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {TestModel} from './interfaces/testModel';
-import admin from 'firebase-admin';
+import {dbTest} from './env/database';
 
 
 const app = express();
 const port = 3000;
-const serviceAccount = require('/Users/mehdi/WebstormProjects/Node.js/src/app/env/key.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://nodejs-af04c.firebaseio.com'
-});
-
-
-const ref = admin.firestore();
-const dbTest = ref.collection('test');
-
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
@@ -27,7 +16,6 @@ app.get('/', async (req, res) => {
     res.status(200).send(userOb);
     console.log(userOb);
 });
-
 
 app.listen(port, function () {
     console.log(`--------------> Écoute sur le port : ${port}`);
