689f216389fa925da3cc4cfb49a5d58ea5fd9319
mehdi <legoullonmehdi@gmail.com>
2020-03-21 20:16:54 +0100

    add Firebase

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import admin from 'firebase-admin';
+
+const serviceAccount = require('src/app/env/key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://node-js-a9f9e.firebaseio.com"
+});
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
+
 app.get('/', async function (req: any, res: any) {
     await res.send('Hello boys!');
 });
-
 
 app.listen(port, function () {
     console.log(`Écoute sur le port : ${port}`);
