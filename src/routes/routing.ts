ff46e7b7b3d8dadcd5ff1e7b19b95fc246b4905b
mehdi <legoullonmehdi@gmail.com>
2020-05-23 00:27:12 +0200

    improve code

--- src/routes/routing.ts
+++ src/routes/routing.ts
@@ -10,24 +10,24 @@ const BASE_URL: string = 'https://www.lemonde.fr/rss/en_continu.xml';
 
 router.get('/', (async (req, res) => {
     const rssFeed = await axios.get(`${BASE_URL}`);
-    const xmL2JS = convert.xml2js(rssFeed.data).elements;
 
-    const tab = Object.values(xmL2JS)
-        .map(value1 => value1 as RssModel)
-        .map(value2 => value2.elements);
-
-    for (let data in tab) {
-        res.send(...Object.values(tab[data] as RssModel)
+    try {
+        const xmL2JS = convert.xml2js(rssFeed.data).elements
             .map(value1 => value1 as RssModel)
-            .map(value2 => Object.values(value2.elements)
-                .splice(7, 10)
-                .map(value1 => value1 as RssModel)
-                .map(value2 => Object.values(value2.elements)
-                    .map(value3 => Object.values(value3))
+            .map(value2 => value2.elements
+                .map((value3: RssModel) => value3.elements)
+                .map(value4 => value4
+                    .slice(7, 8)
                 )
             )
-        )
+
+        for (let data in xmL2JS) {
+            res.send(...Object.values(xmL2JS[data]))
+        }
+    } catch (err) {
+        throw err;
     }
+
 }));
 
 
