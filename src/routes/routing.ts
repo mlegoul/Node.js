044088d06e8a10b9e7bc961fd1233abd3dea7e7f
mehdi <legoullonmehdi@gmail.com>
2020-05-23 13:17:53 +0200

    get 5 objects rss

--- src/routes/routing.ts
+++ src/routes/routing.ts
@@ -9,26 +9,28 @@ const BASE_URL: string = 'https://www.lemonde.fr/rss/en_continu.xml';
 
 
 router.get('/', (async (req, res) => {
-    const rssFeed = await axios.get(`${BASE_URL}`);
+            const rssFeed = await axios.get(`${BASE_URL}`);
 
-    try {
-        const xmL2JS = convert.xml2js(rssFeed.data).elements
-            .map(value1 => value1 as RssModel)
-            .map(value2 => value2.elements
-                .map((value3: RssModel) => value3.elements)
-                .map(value4 => value4
-                    .slice(7, 8)
-                )
-            )
+            try {
+                const xmL2JS = convert.xml2js(rssFeed.data).elements
+                    .map(value1 => value1 as RssModel)
+                    .map(value2 => value2.elements
+                        .map((value3: RssModel) => value3.elements)
+                        .map(value4 => value4
+                            .slice(7, 12)
+                            .map((value4: RssModel) => value4.elements)
+                        )
+                    )
 
-        for (let data in xmL2JS) {
-            res.send(...Object.values(xmL2JS[data]))
+                for (let data in xmL2JS) {
+                    res.send(...Object.values(xmL2JS[data]))
+                }
+            } catch (err) {
+                throw err;
+            }
         }
-    } catch (err) {
-        throw err;
-    }
-
-}));
+    )
+);
 
 
 router.use((req, res) => {
