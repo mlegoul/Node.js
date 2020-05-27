b2da686a740bd00532c9ab10aa9bccf16b92a4bc
mehdi <legoullonmehdi@gmail.com>
2020-05-27 17:20:47 +0200

    clean JSON

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,30 +1,20 @@
-import pg from 'pg';
 import axios from 'axios';
 import convert from 'xml-js';
 import {RssModel} from '../models/rss-model';
 
 
-// Config connect bdd
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    database: 'rss',
-    port: 5432,
-});
-
 const BASE_URL: string = 'https://www.lemonde.fr/rss/en_continu.xml';
-const addJson: string = 'INSERT INTO rss (rss) VALUES ($1)';
 
 async function getJson(req, res) {
 
     const rssFeed = await axios.get(`${BASE_URL}`);
 
-    const xmL2JS = convert.xml2js(rssFeed.data).elements
+    const xmL2JS = convert.xml2js(rssFeed.data)
+        .elements
         .map(value1 => value1.elements
             .map((value2: RssModel) => value2.elements)
             .map(value3 => value3
-                .slice(7, 8)
+                .slice(7, 17)
                 .map((value4: RssModel) => value4.elements)
                 .map(value5 => value5
                     .reduce((acc) => {
@@ -70,26 +60,23 @@ async function getJson(req, res) {
             )
         )
 
-
-    res.status(200).send(...xmL2JS);
+    res.status(200).send(
+        xmL2JS
+            .reduce((acc, value) => {
+                return {
+                    ...acc, ...value
+                        .reduce((acc, value) => {
+                            return {
+                                ...acc, ...value
+                            }
+                        }, {})
+                }
+            }, {})
+    );
 }
 
-async function postJson(req, res) {
-
-    let {json} = req.body;
-
-    pool.query(addJson, [json], (error, results) => {
-
-        if (error) {
-            return res.status(500).send({'ERROR MESSAGE FROM DATABASE : ': error.message});
-        } else {
-            return res.status(201).send({'OK ==> ': results.rows})
-        }
-    });
-
-}
 
 export default {
     getJson,
-    postJson,
 }
+
--- src/routes/routing.ts
+++ src/routes/routing.ts
@@ -5,7 +5,6 @@ const router = express.Router();
 
 
 router.get('/', db.getJson);
-router.post('/api', db.postJson);
 
 router.use((req, res) => {
     return res.status(404).send('<h2 align=center>Page Not Found !</h2>');
