ce7cad0c784d056246de958e334232038c330b1b
mehdi <legoullonmehdi@gmail.com>
2020-06-03 15:40:02 +0200

    check if password match with password DB

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -1,5 +1,6 @@
 import pg from 'pg';
 import bcrypt from 'bcrypt';
+import {AuthModel} from '../interfaces/auth-model';
 
 
 // Config connect bdd
@@ -13,6 +14,41 @@ const pool = new pg.Pool({
 
 async function login(req, res) {
 
+    const searchEmail: string = 'SELECT email FROM users WHERE email = $1';
+
+    const {email} = req.body;
+
+    pool.query(searchEmail, [email], (error, results) => {
+
+        if (error) {
+            return res.status(500).send(error.message);
+        } else {
+            return checkEmailIsValid(req, res);
+        }
+    });
+}
+
+async function checkEmailIsValid(req, res) {
+
+    const hachPassword: string = 'SELECT hached_password FROM users';
+    const {password} = req.body;
+
+
+    pool.query(hachPassword, (err, res) => {
+        if (err) {
+            throw err;
+        } else {
+            const convert = Object.values(res.rows).map((value: AuthModel) => value.hached_password).toString();
+            bcrypt.compare(password, convert, (err, res) => {
+                if(err) {
+                    throw err;
+                } else  {
+                    console.log(res);
+                }
+            })
+
+        }
+    })
 }
 
 
