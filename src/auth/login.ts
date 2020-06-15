a820e659001cb6591b45a27e91357f3af6829961
mehdi <legoullonmehdi@gmail.com>
2020-06-15 19:23:14 +0200

    modify login.ts

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -18,10 +18,12 @@ async function login(req, res) {
         const searchEmail: string = 'SELECT email FROM users WHERE email = $1';
         const {email} = await req.body;
 
-        pool.query(searchEmail, [email], (error, results) => {
+        pool.query(searchEmail, [email], (err, results) => {
 
-            if (error) {
-                return res.status(500).send(error.message);
+            if (err) {
+                return res.status(500).send(err.message);
+            } else if (!results.rows.length) {
+                return res.status(401).send('Error with Email !');
             } else {
                 return checkIsPasswordMatch(req, res);
             }
@@ -33,41 +35,37 @@ async function login(req, res) {
 
 async function checkIsPasswordMatch(req, result) {
 
-    const hachPassword: string = 'SELECT * FROM users';
-    const {password} = await req.body;
+    try {
+        const hachPassword: string = 'SELECT * FROM users';
+        const {password} = await req.body;
 
 
-    pool.query(hachPassword, async (err, res) => {
+        pool.query(hachPassword, async (err, res) => {
 
-            const hached_password: string = Object.values(res.rows)
-                .map((value: AuthModel) => value.hached_password)
-                .toString();
-            const uid: string = Object.values(res.rows)
-                .map((value: AuthModel) => value.id)
-                .toString();
+                const hached_password: string = Object.values(res.rows)
+                    .map((value: AuthModel) => value.hached_password)
+                    .toString();
 
-            const email: string = Object.values(res.rows)
-                .map((value: AuthModel) => value.email)
-                .toString();
-
-            const match = await bcrypt.compare(password, hached_password);
-
-            if (err) {
-                throw err;
-            } else if (!match) {
-                return result.status(401).send('NO MATCH');
-            } else {
                 const token = jwt.sign({
-                    userId: uid,
-                    email: email,
                     algorithm: 'RS256',
                     expiresIn: '1h',
                 }, process.env.TOKEN_SECRET);
 
-                return result.status(200).json({token: token});
+                // Verify if password match
+                const match = await bcrypt.compare(password, hached_password);
+
+                if (err) {
+                    throw err;
+                } else if (!match) {
+                    return result.status(401).send('Password NO MATCH');
+                } else {
+                    return result.status(200).send({token: token});
+                }
             }
-        }
-    )
+        )
+    } catch (err) {
+        throw err;
+    }
 }
 
 
