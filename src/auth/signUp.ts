c0434e262d7971b134bd93b249b1abdef3e39619
mehdi <legoullonmehdi@gmail.com>
2020-06-17 11:10:09 +0200

    add try/catch signUp

--- src/auth/signUp.ts
+++ src/auth/signUp.ts
@@ -24,18 +24,19 @@ async function signUp(req, res) {
 
 
         bcrypt.hash(password, 10, (err, hash) => {
-            if (err) {
+
+            try {
+                pool.query(addNewUser, [email, hash], (error, results) => {
+
+                    if (error) {
+                        return res.status(500).send({'ERROR MESSAGE FROM DATABASE : ': error.message});
+                    } else {
+                        return res.status(201).json(results.rows);
+                    }
+                });
+            } catch (err) {
                 throw err;
             }
-
-            pool.query(addNewUser, [email, hash], (error, results) => {
-
-                if (error) {
-                    return res.status(500).send({'ERROR MESSAGE FROM DATABASE : ': error.message});
-                } else {
-                    return res.status(201).json(results.rows);
-                }
-            });
         })
 
         return res.set({token: token});
